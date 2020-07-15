2c5a96e3fa9462a8d86c52004354e44cd5481eea
Rodrigo Eduardo Lander Sepulveda <Rodrigo.Lander@BUPA.CL>
2020-07-15 11:47:59 -0400

    se agrega variable permiso y luego compara.

--- php/login.php
+++ php/login.php
@@ -6,19 +6,31 @@ if(!empty($_POST)){
 			include "conexion.php";
 			
 			$user_id=null;
+			$permiso = null;
             $dato = md5($_POST[password]);
 			$sql1= "select * from user where (username=\"$_POST[username]\" or email=\"$_POST[username]\") and password=\"$dato\" ";
 			$query = $con->query($sql1);
 			while ($r=$query->fetch_array()) {
 				$user_id=$r["id"];
+				$permiso=$r["type_acc"];
 				break;
 			}
-			if($user_id==null){
-				print "<script>alert(\"Acceso invalido.\");window.location='../login.php';</script>";
-			}else{
+			if($user_id==null)
+				{
+					print "<script>alert(\"Acceso invalido.\");window.location='../login.php';</script>";
+				}
+			else if($permiso=="administrador"){
 				session_start();
 				$_SESSION["user_id"]=$user_id;
-				print "<script>window.location='../home.php';</script>";			
+				//AQUI SE TIENE QUE IR AL MENU ADMINISTRADOR
+				//print "<script>window.location='../home.php';</script>";
+
+				}
+			else{
+				session_start();
+				$_SESSION["user_id"]=$user_id;
+				print "<script>window.location='../home.php';</script>";
+			}			
 			}
 		}
 	}
